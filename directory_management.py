2a7918d1dd064cb87157eb50a192916e0fb514db
RajviDave <rajvidave22@gmail.com>
2026-05-26 14:25:05 +0530

    directory managed part 2

--- directory_management.py
+++ directory_management.py
@@ -1,5 +1,6 @@
-import os
 
-folder_name = f"PH2Dataset\PH2_DATASET_IMAGES"
+from pathlib import Path
+
+folder_name = Path("PH2Dataset/PH2_DATASET_IMAGES")
 for folders in folder_name:
     print(folders)
