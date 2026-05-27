c5f60a297ab462d30dd53334631e6e4cb07314f4
RajviDave <rajvidave22@gmail.com>
2026-05-27 16:23:48 +0530

    second level folder done

--- directory_management.py
+++ directory_management.py
@@ -1,7 +1,5 @@
 import os
-
-# path="PH2Dataset\PH2_DATASET_IMAGES"
-# folders=os.listdir(path)
+import shutil
 
 path="PH2Dataset/PH2_DATASET_IMAGES"
 destination_path="DATASET"
@@ -12,3 +10,9 @@ lesion_path=os.path.join(destination_path,"lesion")
 os.makedirs(dermo_path,exist_ok=True)
 os.makedirs(lesion_path,exist_ok=True)
 
+image_folders=os.listdir(path)
+for image_folders1 in image_folders:
+    path1=os.path.join(path,image_folders1)
+    for image_folder2 in path1:
+        path2=os.path.join(path1,image_folder2)
+        print(path2)
