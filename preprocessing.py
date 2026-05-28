1dabfae2674e6f7f0555dbba0ff5ec4adff5e0e7
RajviDave <rajvidave22@gmail.com>
2026-05-28 17:54:20 +0530

    preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -5,10 +5,17 @@ images={"Images":os.listdir("DATASET/dermoscopic")}
 df=pd.DataFrame(images)
 data=pd.read_excel('PH2Dataset/PH2_dataset.xlsx',skiprows=12)
 data.dropna()
-print(data)
-# C_N=data['Common Nevus'].tolist()
-# A_N=data['Atypical Nevus'].tolist()
-# M=data['Melanoma'].tolist()
+
+C_N=data['Common Nevus'].tolist()
+A_N=data['Atypical Nevus'].tolist()
+M=data['Melanoma'].tolist()
+C_N=pd.Series(C_N)
+A_N=pd.Series(A_N)
+M=pd.Series(M)
+
+C_N.dropna()
+A_N.dropna()
+M.dropna()
 
 
 
