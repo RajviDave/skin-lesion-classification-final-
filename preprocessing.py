b57de81ca47b3b2669a0e55b1a8a6b1122a1d4fb
RajviDave <rajvidave22@gmail.com>
2026-05-28 17:48:57 +0530

    dropped Nan

--- preprocessing.py
+++ preprocessing.py
@@ -4,3 +4,11 @@ import os
 images={"Images":os.listdir("DATASET/dermoscopic")}
 df=pd.DataFrame(images)
 data=pd.read_excel('PH2Dataset/PH2_dataset.xlsx',skiprows=12)
+data.dropna()
+print(data)
+# C_N=data['Common Nevus'].tolist()
+# A_N=data['Atypical Nevus'].tolist()
+# M=data['Melanoma'].tolist()
+
+
+
