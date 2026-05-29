7af2e82125272e189a69828056253bc05adc475a
RajviDave <rajvidave22@gmail.com>
2026-05-29 18:31:47 +0530

    dots and pigmentation

--- simple_model.py
+++ simple_model.py
@@ -6,16 +6,18 @@ data=pd.read_excel('PH2Dataset/PH2_dataset.xlsx',skiprows=12)
 
 print(data.columns)
 df=pd.DataFrame(data['Pigment Network\n(AT/T)'])
-# Dots=data['Dots/Globules\n(A/AT/T)']
+Dots=data['Dots/Globules\n(A/AT/T)']
 # Streaks=data['Streaks\n(A/P)']
 # regression_area=data['Regression Areas\n(A/P)']
 # veil=data['Blue-Whitish Veil\n(A/P)']
-# df['Dots']=Dots
+df['Dots']=Dots
 # df['Streaks']=Streaks
 # df['Regression']=regression_area
 # df['Veil']=veil
 
 encoder=OrdinalEncoder(categories=[['T','AT']])
 df['Pigmentation_Encoded']=encoder.fit_transform(df[['Pigment Network\n(AT/T)']])
-print(df)
 
+encoder=OrdinalEncoder(categories=[['A','T','AT']])
+df['Dots_Encoder']=encoder.fit_transform(df[['Dots/Globules\n(A/AT/T)']])
+print(df)
