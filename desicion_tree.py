025ad09abdca399cc049d1d4425c6f559dda5f97
RajviDave <rajvidave22@gmail.com>
2026-06-04 22:13:28 +0530

    y_prediction

--- desicion_tree.py
+++ desicion_tree.py
@@ -23,3 +23,5 @@ X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.25,random_state=1
 #train desicion tree using gini
 clf_gini=DecisionTreeClassifier(criterion="gini",random_state=100,max_depth=4,min_samples_leaf=3)
 clf_gini.fit(X_train,Y_train)
+y_pred = clf_gini.predict(X_test)
+print(y_pred)
