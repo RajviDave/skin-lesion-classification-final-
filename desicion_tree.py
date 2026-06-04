02ee0a2087c33e41f04809570a2a2ed74a1d0e2e
RajviDave <rajvidave22@gmail.com>
2026-06-04 14:32:56 +0530

    desicion tree

--- desicion_tree.py
+++ desicion_tree.py
@@ -1,4 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from simple_model import encoded_dataframe
+
+
 
