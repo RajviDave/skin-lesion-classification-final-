33ca4c11cbeeb2684f5e4da803e54b76c843254d
RajviDave <rajvidave22@gmail.com>
2026-06-04 22:09:16 +0530

    decision tree gini index training

--- desicion_tree.py
+++ desicion_tree.py
@@ -7,4 +7,19 @@ from simple_model import encoded_dataframe
 
 X=pd.DataFrame()
 X[['Pigment_Encoded','Dots_Encoded','Streaks_Encoded','Regression_Encoded','Veil_Encoded']]=encoded_dataframe[['Pigment_Encoded','Dots_Encoded','Streaks_Encoded','Regression_Encoded','Veil_Encoded']]
-print(X)
+# print(X)
+
+Y=pd.DataFrame()
+Y[['Classes']]=encoded_dataframe[['Classes']]
+# print(len(Y[['Classes']]))
+# print(Y)
+
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.25,random_state=100)
+# print(len(X_train))
+# print(len(Y_train))
+# print(len(X_test))
+# print(len(Y_test))
+
+#train desicion tree using gini
+clf_gini=DecisionTreeClassifier(criterion="gini",random_state=100,max_depth=4,min_samples_leaf=3)
+clf_gini.fit(X_train,Y_train)
