2547c70b85e515dbc26ff35193a65b8deb56d5ea
RajviDave <rajvidave22@gmail.com>
2026-06-04 20:21:59 +0530

    dataframe transfered

--- desicion_tree.py
+++ desicion_tree.py
@@ -5,6 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from simple_model import encoded_dataframe
 
-X=encoded_dataframe.iloc[:3].values
+X=pd.DataFrame()
+X[['Pigment_Encoded','Dots_Encoded','Streaks_Encoded','Regression_Encoded','Veil_Encoded']]=encoded_dataframe[['Pigment_Encoded','Dots_Encoded','Streaks_Encoded','Regression_Encoded','Veil_Encoded']]
 print(X)
-
