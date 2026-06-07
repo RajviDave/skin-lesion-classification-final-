80cf272759030b46ce4d3d95bfd790fe93bb143e
RajviDave <rajvidave22@gmail.com>
2026-06-07 23:09:16 +0530

    cross validation checked

--- svm.py
+++ svm.py
@@ -1,7 +1,7 @@
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import cross_val_score
-from desicion_tree import X_train,Y_train,X_test
+from desicion_tree import X_train,Y_train,X_test,Y_test,X,Y
 
 # Create SVM model
 svm_model = SVC(
@@ -13,3 +13,15 @@ svm_model = SVC(
 
 svm_model.fit(X_train, Y_train.values.ravel())
 y_pred = svm_model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(Y_test, y_pred)
+print("Test Accuracy:", accuracy)
+
+print(classification_report(Y_test, y_pred))
+
+# Cross Validation
+scores = cross_val_score(svm_model, X, Y.values.ravel(), cv=5)
+
+print("CV Scores:", scores)
+print("Mean CV Accuracy:", scores.mean())
