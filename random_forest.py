eaebce9a632002240d50542644d37d792a716876
RajviDave <rajvidave22@gmail.com>
2026-06-09 23:58:32 +0530

    training begins

--- random_forest.py
+++ random_forest.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.model_selection import cross_val_score
+from desicion_tree import X_test,X_train,Y_test,Y_train
 
 # Create Random Forest model
 rf_model = RandomForestClassifier(
@@ -10,3 +11,6 @@ rf_model = RandomForestClassifier(
     min_samples_leaf=3,
     random_state=100
 )
+
+# Train
+rf_model.fit(X_train, Y_train.values.ravel())
