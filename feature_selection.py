06e340b03b0530d1fc3328204c36a1624c7fd9f8
RajviDave <rajvidave22@gmail.com>
2026-06-20 15:13:31 +0530

    binary value encoded

--- feature_selection.py
+++ feature_selection.py
@@ -1,1 +1,18 @@
+import pandas as pd
 import numpy as np
+from preprocessing import data
+from preprocessing import C_N
+# print(C_N)
+print(data.columns)
+
+data["Pigment Network\n(AT/T)"]=np.where(data["Pigment Network\n(AT/T)"]=="AT",0,1)
+# print(data["Pigment Network\n(AT/T)"])
+
+data["Streaks\n(A/P)"]=np.where(data["Streaks\n(A/P)"]=="A",0,1)
+# print(data["Streaks\n(A/P)"])
+
+data["Regression Areas\n(A/P)"]=np.where(data["Regression Areas\n(A/P)"]=="A",0,1)
+# print(data["Regression Areas\n(A/P)"])
+
+data["Blue-Whitish Veil\n(A/P)"]=np.where(data["Blue-Whitish Veil\n(A/P)"]=="A",0,1)
+# print(data["Blue-Whitish Veil\n(A/P)"])
