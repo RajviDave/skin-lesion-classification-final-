40c28c01ec26f244d7095f3f45df3afa3c4f6821
RajviDave <rajvidave22@gmail.com>
2026-06-21 12:37:52 +0530

    chi square test done

--- feature_selection.py
+++ feature_selection.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 from preprocessing import data
 from preprocessing import C_N
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import chi2
 # print(C_N)
 # print(data.columns)
 
@@ -21,18 +22,30 @@ data["Blue-Whitish Veil\n(A/P)"]=np.where(data["Blue-Whitish Veil\n(A/P)"]=="A",
 data_frame={"Size":data["Dots/Globules\n(A/AT/T)"]}
 df = pd.DataFrame(data_frame)
 size_mapping = {
-    'A': -1,
-    'AT': 0,
-    'T': 1
+    'A': 0,
+    'AT': 1,
+    'T': 2
 }
 
 df['Size_Encoded'] = df['Size'].map(size_mapping)
-print(df["Size_Encoded"])
+# print(df["Size_Encoded"])
 df['lesion']=C_N
 df['pigment_network']=data["Pigment Network\n(AT/T)"]
 df['streaks']=data["Streaks\n(A/P)"]
 df['regression_area']=data["Regression Areas\n(A/P)"]
 df['blue_white_veil']=data["Blue-Whitish Veil\n(A/P)"]
 df=df.drop(columns=['Size'])
-print(df)
 
+X_train,X_test,Y_train,Y_test=train_test_split(df[['Size_Encoded','pigment_network','streaks','regression_area','blue_white_veil']],df['lesion'],test_size=0.3,random_state=100)
+# print(X_train.head(),X_test.head(),Y_train.head(),Y_test.head())
+
+# print(X_train.isnull().sum())
+# print(X_train)
+f_p_values=chi2(X_train,Y_train)
+# print(f_p_values)
+
+p_values=pd.Series(f_p_values[1])
+p_values.index=X_train.columns
+# print(p_values)
+
+print(p_values.sort_index(ascending=False))
