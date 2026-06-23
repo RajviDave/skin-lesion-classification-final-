0637eeed5867854466454ada1c4d028d2ec49de0
RajviDave <rajvidave22@gmail.com>
2026-06-23 20:33:24 +0530

    Random forest highest accuracy achived

--- random_forest.py
+++ random_forest.py
@@ -7,7 +7,7 @@ from desicion_tree import X_test,X_train,Y_test,Y_train
 rf_model = RandomForestClassifier(
     n_estimators=25,      # number of trees
     criterion='gini',
-    max_depth=4,           # can also try None
+    max_depth=3,           # can also try None
     min_samples_leaf=3,
     random_state=100
 )
@@ -16,9 +16,12 @@ rf_model = RandomForestClassifier(
 rf_model.fit(X_train, Y_train.values.ravel())
 
 # Predict
-y_pred = rf_model.predict(X_test)
+y_pred = rf_model.predict(X_test)#test accuracy
+y_train_pred=rf_model.predict(X_train)#train accuracy
 
 # Accuracy
 accuracy = accuracy_score(Y_test, y_pred)
 print("Test Accuracy:", accuracy)
 
+test_accuracy=accuracy_score(Y_train,y_train_pred)
+print("Training accuracy:",test_accuracy)
